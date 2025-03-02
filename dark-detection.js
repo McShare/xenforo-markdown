4fc619ddb5b4c0b5175680ec8f6a066326a45bb6
Subilan <suyuanliang1995@gmail.com>
2025-03-02 10:36:42 +0800

    add observer

--- dark-detection.js
+++ dark-detection.js
@@ -1,32 +1,51 @@
 function waitFor(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
+	return new Promise(resolve => {
+		if (document.querySelector(selector)) {
+			return resolve(document.querySelector(selector));
+		}
 
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                observer.disconnect();
-                resolve(document.querySelector(selector));
-            }
-        });
+		const observer = new MutationObserver(mutations => {
+			if (document.querySelector(selector)) {
+				observer.disconnect();
+				resolve(document.querySelector(selector));
+			}
+		});
 
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    });
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true
+		});
+	});
 }
 
-waitFor("a[data-original-title='风格选择']").then(() => {
-    console.log('Detecting dark mode...')
-    const __darkIndicator = document.querySelector("a[data-original-title='风格选择']");
-    if (__darkIndicator !== null) {
-        if (__darkIndicator.innerHTML === '暗夜黑') {
-            console.log('[XFMD] Detected dark mode.');
-            document.querySelector('.xfPreview')?.classList.add('dark');
-            document.querySelectorAll('article.message-body').forEach(e => e.classList.add('dark'));
-            document.querySelectorAll('article.resourceBody-main').forEach(e => e.classList.add('dark'));
-        }
-    }
-})
+waitFor("a[class*='js-styleVariationsLink']").then(() => {
+	console.log('Detecting dark mode...');
+	const __darkIndicator = document.querySelector("a[class*='js-styleVariationsLink']");
+	if (__darkIndicator !== null) {
+		if (__darkIndicator.innerHTML.includes('fa-moon')) {
+			console.log('[XFMD] Detected dark mode.');
+			document.querySelector('.xfPreview')?.classList.add('dark');
+			document.querySelectorAll('article.message-body').forEach(e => e.classList.add('dark'));
+			document.querySelectorAll('article.resourceBody-main').forEach(e => e.classList.add('dark'));
+		}
+	}
+
+	let __darkObserver = new MutationObserver(mutations => {
+		mutations.forEach(m => {
+			const __darkIndicator = document.querySelector("a[class*='js-styleVariationsLink']");
+			if (__darkIndicator !== null) {
+				if (__darkIndicator.innerHTML.includes('fa-moon')) {
+					console.log('[XFMD] Detected dark mode.');
+					document.querySelector('.xfPreview')?.classList.add('dark');
+					document.querySelectorAll('article.message-body').forEach(e => e.classList.add('dark'));
+					document.querySelectorAll('article.resourceBody-main').forEach(e => e.classList.add('dark'));
+				}
+			}
+		});
+	});
+
+	__darkObserver.observe(document.querySelector("a[class*='js-styleVariationsLink']"), {
+		childList: true,
+		subtree: true
+	});
+});
